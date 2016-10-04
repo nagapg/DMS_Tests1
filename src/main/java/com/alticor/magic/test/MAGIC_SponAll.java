85203c06d8f65dafa8e03f63fb9db3834056677e
nagapg <aiudde2@amway.com>
2016-10-04 16:47:42 -0400

    Changed the code to Junit

--- src/main/java/com/alticor/magic/test/MAGIC_SponAll.java
+++ src/main/java/com/alticor/magic/test/MAGIC_SponAll.java
@@ -1,15 +1,14 @@
 package com.alticor.magic.test;
-import java.util.List;
 
-import org.testng.annotations.Test;
+import com.alticor.magic.GeneralFunctions;
+import com.alticor.magic.report.Reports;
+import com.relevantcodes.extentreports.LogStatus;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import com.alticor.magic.test.pages.Reports;
-
-import com.relevantcodes.extentreports.LogStatus;
-import com.alticor.magic.GeneralFunctions;
+import java.util.List;
 
 public class MAGIC_SponAll {
 
@@ -19,7 +18,7 @@ public class MAGIC_SponAll {
     final private String sTranscSuccessMsg="Transaction Successfully Submitted";
 	final private String sExpNote="Test Note";
 	
-	@BeforeMethod
+	@Before
 	public void setUp() throws Exception {
 		Reports.setTestCaseName("MAGIC_SponAll");
 		bStatus=GeneralFunctions.openIEBrowser("https://magic-beta:9446/EBS_UI_Web/Magic");
@@ -32,7 +31,7 @@ public class MAGIC_SponAll {
 		}
 
 	
-	  @AfterMethod
+	  @After
       public void tearDown() throws Exception {
 	  Reports.endTest();
 	   }
