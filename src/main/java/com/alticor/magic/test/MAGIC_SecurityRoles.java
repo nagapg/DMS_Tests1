9ebd84759681273c3005c2ea767e0df832782e5a
Naga Prathima Ghanta <Naga.Prathima.Ghanta@Amway.com>
2016-10-05 14:31:41 -0400

    Updated to Junit standards

--- src/main/java/com/alticor/magic/test/MAGIC_SecurityRoles.java
+++ src/main/java/com/alticor/magic/test/MAGIC_SecurityRoles.java
@@ -1,46 +1,46 @@
+/*Open IE 11 and navigate to beta
+ * click on the user name
+ * Capture the user region*/
 package com.alticor.magic.test;
-import java.util.List;
 
-import org.testng.annotations.Test;
+import com.alticor.magic.GeneralFunctions;
+import com.alticor.magic.report.Reports;
+import com.relevantcodes.extentreports.LogStatus;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import com.alticor.magic.test.pages.Reports;
-
-import com.relevantcodes.extentreports.LogStatus;
-import com.alticor.magic.GeneralFunctions;
-
+import java.util.List;
 
 public class MAGIC_SecurityRoles {
 	
-	
-	/** Init variables */
+/** Init variables */
     boolean bStatus=false;
 	
-	
-	@BeforeMethod
-	public void setUp() throws Exception {
+@Before 
+    public void setUp() throws Exception {
 		Reports.setTestCaseName("MAGIC_SecurityRoles");
 		bStatus=GeneralFunctions.openIEBrowser("https://magic-beta:9446/EBS_UI_Web/Magic");
 		if(bStatus){
-        	System.out.println("Application-Pass");
+        	System.out.println("Beta Opened");
         }	
         else{
-        	System.out.println("Application-Fail");
+        	System.out.println("Cannot open beta");
         }
 		}
 
-	
-	  @AfterMethod
-      public void tearDown() throws Exception {
+@After 
+    public void tearDown() throws Exception {
 	  Reports.endTest();
 	   }
 	
-	  @Test
-	  public void test() {
-	  GeneralFunctions.clickElement(By.xpath("//span[text()='Naga Prathima Gha']"));
-      try {
+
+    @Test 
+    public void test() {
+	  GeneralFunctions.clickElementByJavascriptExecutor(By.xpath("//span[text()='Naga Prathima Gha']"));
+    	//GeneralFunctions.clickElementByJavascriptExecutor(By.xpath(".//*[@id='leftPaneTD']/form/div[3]/span"));//
+    	try {
           Thread.sleep(2000);
       } catch (InterruptedException e) {
           // TODO Auto-generated catch block
@@ -52,13 +52,16 @@ public class MAGIC_SecurityRoles {
       String text = table.get(table.size() - 1).findElement(By.xpath("td[2]")).getText();
       System.out.println("The User Region Captured  is:" + text);
       
+      com.alticor.magic.GeneralFunctions.close();
+      
+      //Test Reports//
        if (bStatus) {
           Reports.logResults(LogStatus.PASS, "Test Passed", "MAGIC_SecurityRoles");
 
       } else {
           Reports.logResults(LogStatus.FAIL, "Test Failed", "MAGIC_SecurityRoles");
       }
-      com.alticor.magic.GeneralFunctions.close();
+      
 
 		
 
