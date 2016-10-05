38e1cacfce394078e2d72d48eb1a2f288a0875b0
Naga Prathima Ghanta <Naga.Prathima.Ghanta@Amway.com>
2016-10-05 14:30:28 -0400

    Updated to Junit standards

--- src/main/java/com/alticor/magic/test/MAGIC_MissingDetails.java
+++ src/main/java/com/alticor/magic/test/MAGIC_MissingDetails.java
@@ -3,17 +3,14 @@
  * Click on the amount 
  * Expected:Amount details should display*/
 package com.alticor.magic.test;
-import java.util.List;
 
-import org.testng.annotations.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import com.alticor.magic.test.pages.Reports;
-
-import com.relevantcodes.extentreports.LogStatus;
 import com.alticor.magic.GeneralFunctions;
+import com.alticor.magic.report.Reports;
+import com.relevantcodes.extentreports.LogStatus;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
 
 public class MAGIC_MissingDetails {
 	
@@ -21,7 +18,7 @@ public class MAGIC_MissingDetails {
 	/** Init variables */
     boolean bStatus=false;
     
-    @BeforeMethod
+    @Before
 	public void setUp() throws Exception {
 		Reports.setTestCaseName("MAGIC_MissingDetails");
 		bStatus=GeneralFunctions.openIEBrowser("https://magic-beta:9446/EBS_UI_Web/Magic");
@@ -34,7 +31,7 @@ public class MAGIC_MissingDetails {
 		}
 
 	
-	  @AfterMethod
+	  @After
       public void tearDown() throws Exception {
 	  Reports.endTest();
 	   }
