ed40d26fdd65e580af11126747b6a06f3c5bc2ee
Naga Prathima Ghanta <Naga.Prathima.Ghanta@Amway.com>
2017-02-17 12:18:13 -0500

    Updated Code

--- src/main/java/com/alticor/magic/test/MAGIC_Q1_VolumesTest.java
+++ src/main/java/com/alticor/magic/test/MAGIC_Q1_VolumesTest.java
@@ -11,12 +11,12 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 
  public class MAGIC_Q1_VolumesTest {
-
-	 /** Declare variables */
+ /** Declare variables */
 	    private boolean bStatus=false;
 	    
 	    @Before public void setUp() throws Exception {
 	        Reports.setTestCaseName("MAGIC_Q1_VolumesTest ");
+	        //Open Browser and Navigate to Gamma//
 	       bStatus = GeneralFunctions.openIEBrowser("https://magic-gamma/EBS_UI_Web/Magic");
 	    if (bStatus) {
 	            System.out.println("Navigated to Environment");
@@ -33,69 +33,69 @@ import org.openqa.selenium.By;
 	@Test
 	public void test() {
 		
-		  //Enter Aff//
-		bStatus=GeneralFunctions.enterTextToElement(By.id("selectedAffLeftContainer"), "010");
-		if(bStatus){
-	    	System.out.println("Passed Aff");
-	    }	
-	    else{
-	    	System.out.println("Cannot Pass Aff");
-	    }
-		
-		//enter Abo//
-		bStatus=GeneralFunctions.enterTextToElement(By.id("selectedIboNumber"), "9995");
-		if(bStatus){
-	    	System.out.println("Passed Abo");
-	    }	
-	    else{
-	    	System.out.println("Cannot Pass Abo");
-	    }
-		
-	   bStatus=GeneralFunctions.enterTextToElement(By.id("selectedPeriod"),"012017");
-		if(bStatus){
-	    	System.out.println("Passed Period");
-	    }	
-	    else{
-	    	System.out.println("Cannot Pass Period");
-	    }
-		
-		
-		bStatus=GeneralFunctions.clickElement(By.id("SubmitButton"));
-		if(bStatus){
-	    	System.out.println("Clicked on Submit");
-	    }	
-	    else{
-	    	System.out.println("Cannot Click on Submit");
-	    }
-	  		 try {
-				Thread.sleep(3000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		
-		
-   
-	  
-	//click on Volumes link//
-		  bStatus =GeneralFunctions.clickElement(By.xpath(".//*[@id='quickglancediv']/div[2]/a[1]"));
+		//Enter Aff:010//
+		  bStatus =GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='selectedAffLeftContainer']"),"010");
 		  if (bStatus) {
-	          System.out.println("Navigated to Volumes");
-	      } 
-		  else {
-	          System.out.println("Cannot Navigate to Volumes");
+	    System.out.println("AFF entered");
+	     } else {
+	    System.out.println("Cannot enter AFF");
+	     }
+		  
+		//Enter Abo:9995//
+		bStatus =GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='selectedIboNumber']"),"9995");
+		  if (bStatus) {
+	      System.out.println("ABO entered");
+	     } else {
+	      System.out.println("Cannot enter ABO");
+	     }
+		  
+		  //Enter Period:012017//
+		  bStatus=GeneralFunctions.enterTextToElement(By.id("selectedPeriod"),"012017");
+			if(bStatus){
+	      	System.out.println("Entered Period");
+	      }	
+	      else{
+	      	System.out.println("Cannot Enter Period");
 	      }
 
-		  
+	       //Click on submit//
+			  bStatus =GeneralFunctions.clickElement(By.id("SubmitButton"));
+			  if (bStatus) {
+		        System.out.println("Click on Submit");
+		    } else {
+		        System.out.println("Cannot click on submit");
+		    }
+			  
+			  try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			
+			//Click on Volumes link//
+			  bStatus =GeneralFunctions.clickElement(By.xpath(".//*[@id='collapsibleQuickGlanceContent']/div[3]/a[2]"));
+			  if (bStatus) {
+		          System.out.println("Navigated to Volumes");
+		      } 
+			  else {
+		          System.out.println("Cannot Navigate to Volumes");
+		      }
+
+			  try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			  
 			  //Validating the Expected//
 		       String ExpVal=GeneralFunctions.getTitle(By.xpath(".//*[@id='IboBonusDtlBar']"));
 			   System.out.println("The Output is:"  +ExpVal);
 			  
-
 		com.alticor.magic.GeneralFunctions.close();
 
-			
-			//Reports//
+				//Reports//
 	          if (bStatus) {
 		            Reports.logResults(LogStatus.PASS, "Test Pass", "VolumesTest");
                } 
