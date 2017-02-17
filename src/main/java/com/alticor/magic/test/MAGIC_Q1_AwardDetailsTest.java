fba466853e71008fdf1b952b5bb7a07404326a7d
Naga Prathima Ghanta <Naga.Prathima.Ghanta@Amway.com>
2017-02-17 12:11:47 -0500

    Updated code

--- src/main/java/com/alticor/magic/test/MAGIC_Q1_AwardDetailsTest.java
+++ src/main/java/com/alticor/magic/test/MAGIC_Q1_AwardDetailsTest.java
@@ -1,5 +1,4 @@
 //Expected:Naviagate to  MAGIC Award details Screen//
-
 package com.alticor.magic.test;
 
 import com.alticor.magic.GeneralFunctions;
@@ -12,13 +11,14 @@ import org.openqa.selenium.By;
 
  public class MAGIC_Q1_AwardDetailsTest {
 
-        /** Declare variables */
+   /** Declare variables */
     private boolean bStatus=false;
     
     @Before public void setUp() throws Exception {
         Reports.setTestCaseName("MAGIC_Q1_AwardDetailsTest");
+        //Open Browser Navigate to Gamma//
        bStatus = GeneralFunctions.openIEBrowser("https://magic-gamma/EBS_UI_Web/Magic");
-    if (bStatus) {
+        if (bStatus) {
             System.out.println("Navigated to Environment");
         } else {
             System.out.println("Cannot navigate to Environment");
@@ -29,100 +29,89 @@ import org.openqa.selenium.By;
 	        Reports.endTest();
 	    }
 	
-	  
 @Test
 public void test() {
 	
-	  //Enter Aff//
-	bStatus=GeneralFunctions.enterTextToElement(By.id("selectedAffLeftContainer"), "010");
-	if(bStatus){
-    	System.out.println("Passed Aff");
-    }	
-    else{
-    	System.out.println("Cannot Pass Aff");
-    }
-	
-	//enter Abo//
-	bStatus=GeneralFunctions.enterTextToElement(By.id("selectedIboNumber"), "9995");
-	if(bStatus){
-    	System.out.println("Passed Abo");
-    }	
-    else{
-    	System.out.println("Cannot Pass Abo");
-    }
-	
-   bStatus=GeneralFunctions.enterTextToElement(By.id("selectedPeriod"),"012017");
-	if(bStatus){
-    	System.out.println("Passed Period");
-    }	
-    else{
-    	System.out.println("Cannot Pass Period");
-    }
-	
-	
-	bStatus=GeneralFunctions.clickElement(By.id("SubmitButton"));
-	if(bStatus){
-    	System.out.println("Clicked on Submit");
-    }	
-    else{
-    	System.out.println("Cannot Click on Submit");
-    }
-  
-	 try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	
-	//Inquiry//
-		bStatus=GeneralFunctions.clickElement(By.xpath(".//*[@id='topMenu']/div/a[1]"));
-     if(bStatus){
-     	System.out.println("Click on Inquiry");
-     }	
-     else{
-     	System.out.println("Cannot Click on Inquiry");
-     }
-		
-	  //Inquiry-->Awards//
-			bStatus=GeneralFunctions.clickElement(By.xpath(".//*[@id='SubMenu1']/a[1]/span[1]"));
-	        if(bStatus){
-			System.out.println("Click on Awards");
-	        }	
-	        else{
-	        	System.out.println("Cannot Click on Awards");
-	        }
-			
-	
+	  //Enter Aff:010//
+	  bStatus =GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='selectedAffLeftContainer']"),"010");
+	  if (bStatus) {
+    System.out.println("AFF entered");
+    } else {
+    System.out.println("Cannot enter AFF");
+   }
 	  
-	  //Click on Award Details//
-	    bStatus=GeneralFunctions.clickElement(By.linkText("Award Details"));
+	//Enter Abo:9995//
+	bStatus =GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='selectedIboNumber']"),"9995");
+	  if (bStatus) {
+      System.out.println("ABO entered");
+      } else {
+      System.out.println("Cannot enter ABO");
+     }
+	  
+  //Enter Period:012017//
+  bStatus=GeneralFunctions.enterTextToElement(By.id("selectedPeriod"),"012017");
+		if(bStatus){
+      	System.out.println("Entered Period");
+      }	
+      else{
+      	System.out.println("Cannot Enter Period");
+      }
+
+    //Click on submit//
+		  bStatus =GeneralFunctions.clickElement(By.id("SubmitButton"));
+		  if (bStatus) {
+	        System.out.println("Click on Submit");
+	    } else {
+	        System.out.println("Cannot click on submit");
+	    }
+		  
+	 try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		   //Inquiry//
+			bStatus=GeneralFunctions.clickElement(By.xpath("//*[@id='topMenu']/div/a[2]"));
 	        if(bStatus){
-			System.out.println("Click on Award Details");
+	        	System.out.println("Click on Inquiry");
 	        }	
 	        else{
-	        	System.out.println("Cannot Click on Award Details");
+	        	System.out.println("Cannot Click on Inquiry");
 	        }
-			
-
-		//Validating the Expected//
-	String ExpVal=GeneralFunctions.getTitle(By.xpath(".//*[@id('AwdHistExpand')/x:div[1]"));
-		System.out.println("The Award Details Screen is "+ ExpVal);
-		
+	   		
+	   	 //Inquiry-->Awards//
+	    bStatus=GeneralFunctions.clickElement(By.xpath(".//*[@id='SubMenu1']/a[1]/span[1]"));
+	   	if(bStatus){
+	   	System.out.println("Click on Awards");
+	   	        }	
+	   	        else{
+	   	        	System.out.println("Cannot Click on Awards");
+	   	        }
+	   			
+	   	 //Click on Award Details//
+		      bStatus=GeneralFunctions.clickElement(By.linkText("Award Details"));
+	   	        if(bStatus){
+	   			System.out.println("Click on Award Details");
+	   	        }	
+	   	        else{
+	   	        	System.out.println("Cannot Click on Award Details");
+	   	        }
+	   			
+         //Validating the Expected//
+	       String ExpVal=GeneralFunctions.getTitle(By.xpath(".//*[@id('AwdHistExpand')/x:div[1]"));
+	      System.out.println("The Award Details Screen is "+ ExpVal);
 	
-		//Close the browser//
- 	  com.alticor.magic.GeneralFunctions.close();
+   		com.alticor.magic.GeneralFunctions.close();
  		
      //Test Reports//
     if (bStatus) {
           Reports.logResults(LogStatus.PASS, "Test Passed", "MAGIC_AwardDetailsTest");
-
-      } else {
+    } else {
           Reports.logResults(LogStatus.FAIL, "Test Failed", "MAGIC_AwardDetailsTest");
       }
-
-		
-}
+   }
 }
 
 
