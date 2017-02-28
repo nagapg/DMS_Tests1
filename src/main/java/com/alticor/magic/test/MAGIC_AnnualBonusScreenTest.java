7d8ae33cf6d6325b7cc15a4655674e8296f72395
Naga Prathima Ghanta <Naga.Prathima.Ghanta@Amway.com>
2017-02-28 12:39:13 -0500

    Baseline Run on PROWL

--- src/main/java/com/alticor/magic/test/MAGIC_AnnualBonusScreenTest.java
+++ src/main/java/com/alticor/magic/test/MAGIC_AnnualBonusScreenTest.java
@@ -1,4 +1,4 @@
-//Prowl Supported Code//
+//Baseline run on Prowl  Supported Code//
 
 package com.alticor.magic.test;
 
@@ -61,24 +61,24 @@ import org.openqa.selenium.support.ui.Wait;
 		  GeneralFunctions.driver=driver;
 		  
 		  //Inquiry//
-			bStatus=GeneralFunctions.clickElement(By.xpath("//*[@id='topMenu']/div/a[2]"));
+		  bStatus=GeneralFunctions.clickElement(By.xpath(".//*[@id='topMenu']/div/a[1]"));
 	        if(bStatus){
 	        	System.out.println("Click on Inquiry");
 	        }	
 	        else{
 	        	System.out.println("Cannot Click on Inquiry");
 	        }
-	   		
-	   	 //Inquiry-->Awards//
-	    bStatus=GeneralFunctions.clickElement(By.xpath(".//*[@id='SubMenu1']/a[1]/span[1]"));
-	   	if(bStatus){
-	   	System.out.println("Click on Awards");
-	   	        }	
-	   	        else{
-	   	        	System.out.println("Cannot Click on Awards");
-	   	        }
-	   			
-	   	 //Click on Award Details//
+	        
+	        //Inquiry-->Awards//
+		    bStatus=GeneralFunctions.clickElement(By.xpath(".//*[@id='SubMenu1']/a[1]/span[1]"));
+		   	if(bStatus){
+		   	System.out.println("Click on Awards");
+		   	        }	
+		   	        else{
+		   	        	System.out.println("Cannot Click on Awards");
+		   	        }
+		   			
+			 //Click on Award Details//
 		      bStatus=GeneralFunctions.clickElement(By.linkText("Award Details"));
 	   	        if(bStatus){
 	   			System.out.println("Click on Award Details");
@@ -87,48 +87,12 @@ import org.openqa.selenium.support.ui.Wait;
 	   	        	System.out.println("Cannot Click on Award Details");
 	   	        }
 	   			
-       //Validating the Expected//
-	       String ExpVal=GeneralFunctions.getTitle(By.xpath(".//*[@id('AwdHistExpand')/x:div[1]"));
-	      System.out.println("The Award Details Screen is "+ ExpVal);
+	   	     //Validating the Expected//
+	 	       String ExpVal=GeneralFunctions.getTitle(By.xpath(".//*[@id('AwdHistExpand')/x:div[1]"));
+	 	      System.out.println("The Award Details Screen is "+ ExpVal);
 	
 			
-		/*   //Inquiry//
-			GeneralFunctions.clickElement(By.xpath(".//*[@id='topMenu']/div/a[1]"));
-			if(bStatus){
-	        	System.out.println("Inquiry displayed");
-	        }	
-	        else{
-	        	System.out.println("Cannot display Inquiry");
-	        }
-			
-			//Inquiry-->Annuals//
-			GeneralFunctions.clickElement(By.xpath("//span[text()='Annuals']"));
-			if(bStatus){
-	        	System.out.println("Annuals displayed");
-	        }	
-	        else{
-	        	System.out.println("Cannot display Annuals");
-	        }
-			
-			//Inquiry-->Annuals-->Annual Bonus//
-			bStatus=GeneralFunctions.clickElement(By.linkText("Annual Bonus"));
-	        if(bStatus){
-			System.out.println("Annual Bonus displayed");
-	        }	
-	        else{
-	        	System.out.println("Cannot display Annual Bonus");
-	        }
-
-	        //Validating the Expected//
-	        bStatus=GeneralFunctions.display(By.xpath(".//*[@id='searchBoxDiv']/form/div[1]"));
-		     if(bStatus){
-						System.out.println("Expected:Annual Bonus Screen is displayed");
-				       }	
-				       else{
-				       	System.out.println("Actual:Cannot Load Annual Bonus Screen");
-				       }*/
-		     
-		     com.alticor.magic.GeneralFunctions.close();
+			     com.alticor.magic.GeneralFunctions.close();
 				
 				//Test Reports//
 				 if (bStatus) {
