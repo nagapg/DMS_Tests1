841d7f82a79d25dede5c254fbfbd88692e33fd5d
Naga Prathima Ghanta <Naga.Prathima.Ghanta@Amway.com>
2017-04-03 15:19:29 -0400

    Update RunnerTest.java

--- src/main/java/runner/RunnerTest.java
+++ src/main/java/runner/RunnerTest.java
@@ -7,7 +7,9 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/cucumber/MAGICAppl.feature",glue={"stepDefinition"})
+@CucumberOptions(features="src/test/resources/cucumber/MAGICAppl.feature",
+                 glue={"stepDefinition"},
+                plugin={"html:target/cucumber-html-report"})
 
 
 public class RunnerTest {
