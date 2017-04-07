80205b7dc7be821b25700bd079e48ee7f9312d5c
Naga Prathima Ghanta <Naga.Prathima.Ghanta@Amway.com>
2017-04-07 14:56:29 -0400

    Create MA9732.java

---
+++ src/main/java/runner/MA9732.java
@@ -0,0 +1,11 @@
+package runner;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features={"src/test/resources/cucumber/MA9732.feature"},
+glue={"stepDefinition"},
+plugin={"html:target/cucumber-html-report"})
+		
