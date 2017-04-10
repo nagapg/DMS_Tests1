4c3b9eb1c1649f8f0a3a934c77a3c1457a5a645f
Naga Prathima Ghanta <Naga.Prathima.Ghanta@Amway.com>
2017-04-10 09:43:04 -0400

    Update MAGIC_Q2_BaselineRerunTest.java

--- src/main/java/com/alticor/magic/test/MAGIC_Q2_BaselineRerunTest.java
+++ src/main/java/com/alticor/magic/test/MAGIC_Q2_BaselineRerunTest.java
@@ -1,78 +1,61 @@
 package com.alticor.magic.test;
 
 import com.alticor.magic.GeneralFunctions;
-import com.alticor.magic.report.Reports;
+import com.alticor.magic.pages.MagicPage;
+import com.alticor.magic.test.pages.Reports;
+import com.alticor.okta.OktaLogin;
+import com.alticor.prowl.AmwayProwlRunner;
+import com.alticor.prowl.WebDriverManager;
+import com.alticor.prowl.provider.EndpointProvider;
 import com.relevantcodes.extentreports.LogStatus;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
-public class MAGIC_Q2_BaselineRerunTest {
+@RunWith(AmwayProwlRunner.class) public class MAGIC_Q2_BaselineRerunTest {
 	
-	 /** Declare variables */
-    private boolean bStatus=false;
-    
-    @Before public void setUp() throws Exception {
-        Reports.setTestCaseName("MAGIC_Q2_BaselineRerunTest");
-        
-        //Open Browser Navigate to Gamma//
-       bStatus = GeneralFunctions.openIEBrowser("https://magic-gamma/EBS_UI_Web/Magic");
-        if (bStatus) {
-            System.out.println("Navigated to Environment");
-        } else {
-            System.out.println("Cannot navigate to Environment");
-        }
+    static WebDriver driver;
+    static MagicPage magicPage;
+    static OktaLogin oktaLogin;
+	
+@Rule public ErrorCollector collector = new ErrorCollector();
+
+@BeforeClass public static void setup() {
+    	
+    	Reports.setTestCaseName("MAGIC_Q2_BaselineRerunTest");
+        String baseUrl = EndpointProvider.getEndpoint();
+        driver = WebDriverManager.getInstance().getDriver();
+
+        oktaLogin = new OktaLogin(driver, null);
+        oktaLogin.login("cmns559",
+            "our49pot");// TODO: Make this grab password from environment variables
+        driver.getTitle();
+
+        magicPage = new MagicPage(driver, null);
+        magicPage.setBaseUrl(baseUrl);
+        magicPage.aboLookup("430","46","042017");
     }
-  
-	    @After public void afterTest() {
-	        Reports.endTest();
-	    }
-	
-@Test
-public void test() {
-	
-	  //Enter Aff:430//
-	  bStatus =GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='selectedAffLeftContainer']"),"430");
-	  if (bStatus) {
-    System.out.println("AFF entered");
-    } else {
-    System.out.println("Cannot enter AFF");
-   }
-	  
-	//Enter Abo:46//
-	bStatus =GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='selectedIboNumber']"),"46");
-	  if (bStatus) {
-      System.out.println("ABO entered");
-      } else {
-      System.out.println("Cannot enter ABO");
-     }
-	  
-  //Enter Period:042017//
-  bStatus=GeneralFunctions.enterTextToElement(By.id("selectedPeriod"),"042017");
-		if(bStatus){
-      	System.out.println("Entered Period");
-      }	
-      else{
-      	System.out.println("Cannot Enter Period");
-      }
 
-    //Click on submit//
-		  bStatus =GeneralFunctions.clickElement(By.id("SubmitButton"));
-		  if (bStatus) {
-	        System.out.println("Click on Submit");
-	    } else {
-	        System.out.println("Cannot click on submit");
-	    }
-		  
-	 try {
-				Thread.sleep(3000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		
-		//Maintenance//
+@AfterClass public static void tearDown() {
+	    
+        WebDriverManager.getInstance().close();
+        Reports.endTest();
+}
+
+/** Declare variables */
+	boolean bStatus;
+
+@Test
+	  public void test() {
+
+ GeneralFunctions.driver=driver;
+	
+	   //Maintenance//
 		bStatus=GeneralFunctions.clickElement(By.xpath(".//*[@id='topMenu']/div/a[3]"));
 		if(bStatus){
      	System.out.println("Click on Maintanence");
@@ -80,8 +63,8 @@ public void test() {
      else{
      	System.out.println("Cannot Click on Maintanence");
      }
-		
-		//Maintenance-->GIP//
+
+	//Maintenance-->GIP//
 		bStatus=GeneralFunctions.clickElementByJavascriptExecutor(By.xpath(".//*[@id='SubMenu2']/a[14]/span[1]"));
 		if(bStatus){
      	System.out.println("Click on GIP");
@@ -89,9 +72,9 @@ public void test() {
      else{
      	System.out.println("Cannot Click on GIP");
      }
-		
-	  //Maintenance-->GIP-->Baseline Rerun//
-     bStatus=GeneralFunctions.clickElement(By.linkText("Baseline Rerun"));   
+
+      //Maintenance-->GIP-->Baseline Rerun//
+      bStatus=GeneralFunctions.clickElement(By.linkText("Baseline Rerun"));   
     if(bStatus){
 		System.out.println("Click on Baseline Rerun");
     }	
@@ -99,16 +82,16 @@ public void test() {
     	System.out.println("Cannot Click on Baseline Rerun");
     }
     
-
-    bStatus=GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='effPerfYear']"),"2016");
+		  //Enter effPY:2016//
+bStatus=GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='effPerfYear']"),"2016");
 if(bStatus){
 	System.out.println("Enter Eff PY");
 }	
 else{
 	System.out.println("Cannot Enter Eff PY");
 }
-
-
+		  
+//Click on Baseline Rerun button//
 bStatus=GeneralFunctions.clickElement(By.id("rerunBaselineButton"));
 if(bStatus){
 	System.out.println("Click on Submit Rerun");
@@ -123,19 +106,19 @@ try {
 	e.printStackTrace();
 }
 
-//Capture the Result1//
-String ExpVal2=GeneralFunctions.getText(By.xpath(".//*[@id='successMsgDiv']"));
-  System.out.println("The Output is:"  +ExpVal2);
+//Capture the Result//
+String ExpVal=GeneralFunctions.getText(By.xpath(".//*[@id='successMsgDiv']"));
+  System.out.println("The Output is:"  +ExpVal);
 
+		  //Close the browser//
 com.alticor.magic.GeneralFunctions.close();
-	
-  //Test Reports//
- if (bStatus) {
-       Reports.logResults(LogStatus.PASS, "Test Passed", "MAGIC_Q2_BaselineRerunTest");
- } else {
-       Reports.logResults(LogStatus.FAIL, "Test Failed", "MAGIC_Q2_BaselineRerunTest");
-   }
-}
-}
-
-
+	   			
+	   //Test Reports//
+	   if (bStatus) {
+	   Reports.logResults(LogStatus.PASS, "Test Pass","MAGIC_Q2_BaselineRerunTest");
+	   } 
+		  else {
+	    Reports.logResults(LogStatus.FAIL, "Test Fail","MAGIC_Q2_BaselineRerunTest");
+	   		        }
+	   	}
+	   	}
