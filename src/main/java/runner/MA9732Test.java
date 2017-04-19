3b0269697a1366d9bbf2d0d1d60a06bfc21496d8
nagapg <aiudde2@amway.com>
2017-04-19 11:05:08 -0400

    Multiple changes

---
+++ src/main/java/runner/MA9732Test.java
@@ -0,0 +1,13 @@
+package runner;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features={"src/test/resources/cucumber/MA9732.feature"},
+glue={"com.alticor.magic.test"},
+plugin={"html:target/cucumber-html-report"})
+		
+public class MA9732Test {
+}
