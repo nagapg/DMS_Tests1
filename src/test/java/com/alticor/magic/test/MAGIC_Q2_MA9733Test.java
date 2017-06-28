e6478ac00643595e7a05a4bc2a968b73a7794312
nagapg <aiudde2@amway.com>
2017-06-28 13:47:17 -0400

    No Change

--- src/test/java/com/alticor/magic/test/MAGIC_Q2_MA9733Test.java
+++ src/test/java/com/alticor/magic/test/MAGIC_Q2_MA9733Test.java
@@ -46,14 +46,19 @@ public class MAGIC_Q2_MA9733Test {
 				    GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='okta-signin-username']"),"cmns559");
 				    GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='okta-signin-password']"),"our49pot");
 				    GeneralFunctions.clickElement(By.xpath(".//*[@id='okta-signin-submit']"));
-				 
+				    try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}	 
 				     //Enter Aff//
 				    try {
 						Thread.sleep(3000);
 					} catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
-					}
+					}	   
 	    bStatus =GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='selectedAffLeftContainer']"),"010");
 		  if (bStatus) {
 		System.out.println("AFF-Pass");
@@ -68,7 +73,7 @@ public class MAGIC_Q2_MA9733Test {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		bStatus =GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='selectedIboNumber']"),"9995");
+		bStatus =GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='selectedIboNumber']"),"670");
 		  if (bStatus) {
 		System.out.println("ABO-Pass");
 		} else {
@@ -91,8 +96,7 @@ public class MAGIC_Q2_MA9733Test {
 		}
 		 
 		   		//Click on submit//
-		               
-		                 bStatus =GeneralFunctions.clickElement(By.id("SubmitButton"));
+		               bStatus =GeneralFunctions.clickElement(By.id("SubmitButton"));
 		                 if (bStatus) {
 		        System.out.println("Submit-Pass");
 		    } else {
