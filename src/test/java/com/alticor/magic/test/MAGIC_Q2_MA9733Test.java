3d37a65796acc5d1e08d507ceeb9f0ead1e3ba74
nagapg <aiudde2@amway.com>
2017-07-06 13:29:38 -0400

    Removed gaps

--- src/test/java/com/alticor/magic/test/MAGIC_Q2_MA9733Test.java
+++ src/test/java/com/alticor/magic/test/MAGIC_Q2_MA9733Test.java
@@ -6,7 +6,6 @@ package com.alticor.magic.test;
 
 import org.openqa.selenium.By;
 import com.alticor.magic.GeneralFunctions;
-
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
