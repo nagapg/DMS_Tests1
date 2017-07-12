fe04bd2103f41a42610641d19f0bc6e32a36ab72
nagapg <aiudde2@amway.com>
2017-07-12 11:10:07 -0400

    Code clean up

--- src/test/java/com/alticor/magic/test/MAGIC_Q2_MA9738Test.java
+++ src/test/java/com/alticor/magic/test/MAGIC_Q2_MA9738Test.java
@@ -6,6 +6,8 @@ package com.alticor.magic.test;
 
 import org.openqa.selenium.By;
 import com.alticor.magic.GeneralFunctions;
+
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
@@ -14,11 +16,110 @@ public class MAGIC_Q2_MA9738Test {
 	/** Declare variables */
 	boolean bStatus;
 	
-	@When("^navigated to ILB Bank File$")
-	public void navigated_to_ILB_Bank_File() throws Throwable {
+	@Given("^user navigates to ILB Bank File$")
+	public void user_navigates_to_ILB_Bank_File() throws Throwable {
+		
+		//Navigate to Server//
+		bStatus =GeneralFunctions.openhubBrowser("http://10.72.16.9:32768/wd/hub","firefox");
+		if (bStatus) {
+				    System.out.println("Server Firefox-Pass");
+				     } else {
+				     System.out.println("Server Firefox-Fail");
+				     }
+					
+					//Navigate to MAGIC-QA//
+				 bStatus =GeneralFunctions.openapp("https://magic-gamma/EBS_UI_Web/Magic");
+				  if (bStatus) {
+				    System.out.println("MAGIC_QA-Pass");
+				    } else {
+				    System.out.println("MAGIC_QA-Fail");
+				    }
+				 
+				  try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				 
+				    //Okta Login//
+				    GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='okta-signin-username']"),"cmns559");
+				    GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='okta-signin-password']"),"our49pot");
+				    GeneralFunctions.clickElement(By.xpath(".//*[@id='okta-signin-submit']"));
+				    try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+	
+//Enter Aff//
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	    bStatus =GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='selectedAffLeftContainer']"),"010");
+		  if (bStatus) {
+		System.out.println("AFF-Pass");
+		} else {
+		System.out.println("AFF-Fail");
+		}
+ try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		  
+			  //Enter Abo//
+		bStatus =GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='selectedIboNumber']"),"9995");
+		  if (bStatus) {
+		System.out.println("ABO-Pass");
+		} else {
+		System.out.println("ABO-Fail");
+		}
+		    try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		  
+			   //Enter Period//
+		bStatus=GeneralFunctions.enterTextToElement(By.id("selectedPeriod"),"062017");
+		               if(bStatus){
+		System.out.println("Period-Pass");
+		}         
+		else{
+		System.out.println("Period-Fail");
+		}
+		 
+		               try {
+		   				Thread.sleep(3000);
+		   			} catch (InterruptedException e) {
+		   				// TODO Auto-generated catch block
+		   				e.printStackTrace();
+		   			}
+		   		
+		       //Click on submit//
+		                 bStatus =GeneralFunctions.clickElement(By.id("SubmitButton"));
+		                 if (bStatus) {
+		        System.out.println("Submit-Pass");
+		    } else {
+		        System.out.println("Submit-Fail");
+		    }
+		                 
+		                 try {
+		     				Thread.sleep(3000);
+		     			} catch (InterruptedException e) {
+		     				// TODO Auto-generated catch block
+		     				e.printStackTrace();
+		     			}
 		
 	//Processing//
-		GeneralFunctions.waittime();
 		 bStatus=GeneralFunctions.clickElement(By.xpath(".//*[@id='topMenu']/div/a[5]"));
 			       			if(bStatus){
 			       	        	System.out.println("Processing-Pass");
@@ -28,7 +129,6 @@ public class MAGIC_Q2_MA9738Test {
 			       	        }
 			       			
 			       			//Processing-->payments//
-			       			GeneralFunctions.waittime();
 			       			bStatus=GeneralFunctions.clickElement(By.xpath(".//*[@id='SubMenu5']/a[7]/span[1]"));
 			       			if(bStatus){
 			       	        	System.out.println("Payments-Pass");
@@ -38,8 +138,7 @@ public class MAGIC_Q2_MA9738Test {
 			       	        }
 			       			
 			       			//Processing-->Payments-->ILB bank file//
-			       			GeneralFunctions.waittime();
-			       			bStatus=GeneralFunctions.clickElementByJavascriptExecutor(By.linkText("ILB Bank File"));
+			       				bStatus=GeneralFunctions.clickElementByJavascriptExecutor(By.linkText("ILB Bank File"));
 			       			if(bStatus){
 			       	        	System.out.println("ILB Bank File-Pass");
 			       	        }	
@@ -48,7 +147,12 @@ public class MAGIC_Q2_MA9738Test {
 			       	        }
 			       			
 			       			//Enter Period//
-			       			GeneralFunctions.waittime();
+			       		  try {
+			  				Thread.sleep(3000);
+			  			} catch (InterruptedException e) {
+			  				// TODO Auto-generated catch block
+			  				e.printStackTrace();
+			  			}
 			       	bStatus=GeneralFunctions.enterTextToElement(By.name("period"),"022015");
 				               if(bStatus){
 				System.out.println("Period-Pass");
@@ -65,14 +169,24 @@ public class MAGIC_Q2_MA9738Test {
 			    	        else{
 			    	        System.out.println("Go-Fail");
 			    	        }
-			    			GeneralFunctions.waittime();
+			    			  try {
+			    					Thread.sleep(3000);
+			    				} catch (InterruptedException e) {
+			    					// TODO Auto-generated catch block
+			    					e.printStackTrace();
+			    				}
 			    			}
 
 	@When("^Transmitted links selected$")
 	public void Transmitted_links_selected() throws Throwable {
 		
 	//Click on Transmitted link//
-		GeneralFunctions.waittime();
+		  try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		bStatus=GeneralFunctions.clickElement(By.linkText("Transmitted"));
 		if(bStatus){
         	System.out.println("Click Transmitted Link-Pass");
@@ -80,7 +194,12 @@ public class MAGIC_Q2_MA9738Test {
         else{
         System.out.println("Click Transmitted Link-Fail");
         }
-		GeneralFunctions.waittime();
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	 
 		//Click on Second Transmitted link//
 		  bStatus=GeneralFunctions.clickElement(By.xpath(".//*[@id='searchForm']/table/tbody/tr[2]/td[2]/span/b"));
@@ -90,12 +209,24 @@ public class MAGIC_Q2_MA9738Test {
 	        else{
 	        System.out.println("Second Transmitted Link-Fail");
 	        }
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			}
 
 	@Then("^view audit details$")
 	public void view_audit_details() throws Throwable {
 		
-		GeneralFunctions.waittime();
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	     //Expected:View Audit Details//
 				bStatus=GeneralFunctions.clickElement(By.xpath(".//*[@id='divAuditDetails0']/table/tbody/tr[1]/td/table/tbody/tr/td[1]/div"));
 				if(bStatus){
@@ -123,4 +254,15 @@ public class MAGIC_Q2_MA9738Test {
          	System.out.println("Click Back-Fail");
          }
    	 	}
-}
+
+	@Then("^Close ILB Bank File$")
+	public void  Close_ILB_Bank_File() throws Throwable {
+	  
+	  	bStatus = GeneralFunctions.close();
+	      if (bStatus) {
+	           System.out.println("MAGIC Closed-Pass");
+	       } else {
+	           System.out.println("Close MAGIC-Fail");
+	       }
+	}
+	}
--- src/test/resources/cucumber/MA9738.feature
+++ src/test/resources/cucumber/MA9738.feature
@@ -1,13 +1,7 @@
 Feature: ILB Bank File
 
-Scenario Outline:  
-         Given user navigates to GAMMA
-         And "<aff>" "<abo>" "<period>" entered and click on submit 
-         When navigated to ILB Bank File
-       And Transmitted links selected
+Scenario:  
+       Given user navigates to ILB Bank File
+       When Transmitted links selected
       Then view audit details
-      And Close Gamma
-         
-         Examples:
-         | aff | abo | period |
-         | 010 | 9995 | 062017 |
+      And Close ILB Bank File
