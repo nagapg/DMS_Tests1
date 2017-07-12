a9434d0a729a940aeb96ccf2a355dc1e47eff2cb
nagapg <aiudde2@amway.com>
2017-07-12 11:04:51 -0400

    Code clean up

--- src/test/java/com/alticor/magic/test/MAGIC_Q2_MA9737Test.java
+++ src/test/java/com/alticor/magic/test/MAGIC_Q2_MA9737Test.java
@@ -6,6 +6,8 @@ package com.alticor.magic.test;
 
 import org.openqa.selenium.By;
 import com.alticor.magic.GeneralFunctions;
+
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
@@ -14,11 +16,100 @@ public class MAGIC_Q2_MA9737Test {
 	/** Declare variables */
 	boolean bStatus;
 	
-	@When("^navigated to ILB Alt Name Screen$")
-	public void navigated_to_ILB_Alt_Name_Screen() throws Throwable {
+	@Given("^user navigates to ILB Alt Name$")
+	public void user_navigates_to_ILB_Alt_Name() throws Throwable {
 		
-		//Reports//
- GeneralFunctions.waittime();
+		//Navigate to Server//
+		bStatus =GeneralFunctions.openhubBrowser("http://10.72.16.9:32768/wd/hub","firefox");
+		if (bStatus) {
+				    System.out.println("Server Firefox-Pass");
+				     } else {
+				     System.out.println("Server Firefox-Fail");
+				     }
+					
+					//Navigate to MAGIC-QA//
+				 bStatus =GeneralFunctions.openapp("https://magic-gamma/EBS_UI_Web/Magic");
+				  if (bStatus) {
+				    System.out.println("MAGIC_QA-Pass");
+				    } else {
+				    System.out.println("MAGIC_QA-Fail");
+				    }
+				  try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				  
+				    //Okta Login//
+				    GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='okta-signin-username']"),"cmns559");
+				    GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='okta-signin-password']"),"our49pot");
+				    GeneralFunctions.clickElement(By.xpath(".//*[@id='okta-signin-submit']"));
+				    try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}	 
+				    
+				     //Enter Aff//
+				    try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}	   
+	    bStatus =GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='selectedAffLeftContainer']"),"010");
+		  if (bStatus) {
+		System.out.println("AFF-Pass");
+		} else {
+		System.out.println("AFF-Fail");
+		}
+
+		  //Enter Abo//
+		  try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		bStatus =GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='selectedIboNumber']"),"670");
+		  if (bStatus) {
+		System.out.println("ABO-Pass");
+		} else {
+		System.out.println("ABO-Fail");
+		}
+		  
+		   //Enter Period//
+		  try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		bStatus=GeneralFunctions.enterTextToElement(By.id("selectedPeriod"),"062017");
+		               if(bStatus){
+		System.out.println("Period-Pass");
+		}         
+		else{
+		System.out.println("Period-Fail");
+		}
+		 
+		   		//Click on submit//
+		               bStatus =GeneralFunctions.clickElement(By.id("SubmitButton"));
+		                 if (bStatus) {
+		        System.out.println("Submit-Pass");
+		    } else {
+		        System.out.println("Submit-Fail");
+		    }
+		              
+	//Reports//
+		                 try {
+								Thread.sleep(3000);
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
 bStatus=GeneralFunctions.clickElement(By.xpath(".//*[@id='topMenu']/div/a[4]"));
 if(bStatus){
 	System.out.println("Reports-Pass");
@@ -28,7 +119,12 @@ else{
 }
 
 //Reports-->ABO Reports//
-GeneralFunctions.waittime();
+try {
+	Thread.sleep(3000);
+} catch (InterruptedException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
 bStatus=GeneralFunctions.clickElement(By.xpath(".//*[@id='SubMenu4']/a[2]/span[1]"));
 if(bStatus){
 	System.out.println("ABO Reports-Pass");
@@ -38,7 +134,12 @@ else{
 }
 
 //Reports-->ABO Reports-->ILB Alt Name//
-GeneralFunctions.waittime();
+try {
+	Thread.sleep(3000);
+} catch (InterruptedException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
 bStatus=GeneralFunctions.clickElement(By.linkText("ILB Alt Name"));
 if(bStatus){
 	System.out.println("ILB Alt name-Pass");
@@ -51,22 +152,10 @@ else{
 	@When("^BE should be empty$")
 	public void BE_should_be_empty() throws Throwable {
 		   
-		   /*	bStatus=(GeneralFunctions.driver.findElement(By.xpath(".//*[@id='searchInputCol2']")).getAttribute("value").isEmpty());
-		if(bStatus)
-		{
-			System.out.println("BE Field Empty-Pass");
-		}
-		else
-		{
-			System.out.println("BE not Empty-Fail");
-		}*/
-		   
 		   //Capture BE Field//
-		GeneralFunctions.waittime();
 			String ExpVal=GeneralFunctions.getText(By.id(".//*[@id='searchInputCol2']"));
 			 System.out.println("##########Expected-Result########################");
-			// System.out.println("The BE Field  is empty");//
-			 System.out.println("The BE Field  is:" + ExpVal );
+			 System.out.println("The BE Field  is empty");
 			 System.out.println("###############################################");
 		}
 
@@ -81,4 +170,18 @@ else{
 		System.out.println("BE Drop Down List not Visible-Fail");
 		}
 	}
-}
+
+	@Then("^Close ILB Alt Name$")
+	public void  Close_ILB_Alt_Name() throws Throwable {
+	  
+	  	bStatus = GeneralFunctions.close();
+	      if (bStatus) {
+	           System.out.println("MAGIC Closed-Pass");
+	       } else {
+	           System.out.println("Close MAGIC-Fail");
+	       }
+	}
+	}
+
+
+
--- src/test/resources/cucumber/MA9737.feature
+++ src/test/resources/cucumber/MA9737.feature
@@ -1,15 +1,9 @@
 Feature: ILB Alt Name Screen
 
-Scenario Outline:  
-         Given user navigates to GAMMA
-         And "<aff>" "<abo>" "<period>" entered and click on submit 
-         When navigated to ILB Alt Name Screen
-       When BE should be empty
-       Then Drop down in BE
-       Then Close Gamma
-         
-         Examples:
-         | aff | abo | period |
-         | 010 | 670 | 062017 |
+Scenario:
+      Given user navigates to ILB Alt Name 
+      When BE should be empty
+      Then Drop down in BE
+      And Close ILB Alt Name 
        
        
