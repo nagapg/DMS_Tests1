f3302f347ad95ca9d9bf892f3baf4a4347dd96ac
nagapg <aiudde2@amway.com>
2017-07-12 11:31:56 -0400

    Code clean up

--- src/test/java/com/alticor/magic/test/MAGIC_Q2_MA9741Test.java
+++ src/test/java/com/alticor/magic/test/MAGIC_Q2_MA9741Test.java
@@ -6,6 +6,7 @@ package com.alticor.magic.test;
 
 import org.openqa.selenium.By;
 import com.alticor.magic.GeneralFunctions;
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
@@ -14,11 +15,99 @@ public class MAGIC_Q2_MA9741Test {
 	/** Declare variables */
 	boolean bStatus;
 	
-	@When("^in Invoice Inquiry Screen$")
-	public void in_Invoice_Inquiry_Screen() throws Throwable {
+	@Given("^user is in Invoice Inquiry$")
+	public void user_is_in_Invoice_Inquiry() throws Throwable {
 		
+		//Navigate to Server//
+		bStatus =GeneralFunctions.openhubBrowser("http://10.72.16.9:32768/wd/hub","firefox");
+		if (bStatus) {
+				    System.out.println("Server Firefox-Pass");
+				     } else {
+				     System.out.println("Server Firefox-Fail");
+				     }
+					
+					//Navigate to MAGIC-QA//
+				 bStatus =GeneralFunctions.openapp("https://magic-gamma/EBS_UI_Web/Magic");
+				  if (bStatus) {
+				    System.out.println("MAGIC_QA-Pass");
+				    } else {
+				    System.out.println("MAGIC_QA-Fail");
+				    }
+				  try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				 
+				    //Okta Login//
+				    GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='okta-signin-username']"),"cmns559");
+				    GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='okta-signin-password']"),"our49pot");
+				    GeneralFunctions.clickElement(By.xpath(".//*[@id='okta-signin-submit']"));
+				    try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}	 
+				     //Enter Aff//
+				    try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}	   
+	    bStatus =GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='selectedAffLeftContainer']"),"010");
+		  if (bStatus) {
+		System.out.println("AFF-Pass");
+		} else {
+		System.out.println("AFF-Fail");
+		}
+
+		  //Enter Abo//
+		  try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		bStatus =GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='selectedIboNumber']"),"670");
+		  if (bStatus) {
+		System.out.println("ABO-Pass");
+		} else {
+		System.out.println("ABO-Fail");
+		}
+		  
+		   //Enter Period//
+		  try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		bStatus=GeneralFunctions.enterTextToElement(By.id("selectedPeriod"),"072017");
+		               if(bStatus){
+		System.out.println("Period-Pass");
+		}         
+		else{
+		System.out.println("Period-Fail");
+		}
+		 
+		   		//Click on submit//
+		               bStatus =GeneralFunctions.clickElement(By.id("SubmitButton"));
+		                 if (bStatus) {
+		        System.out.println("Submit-Pass");
+		    } else {
+		        System.out.println("Submit-Fail");
+		    }
+		              
 				//Click on Payment Inquiry//
-		                GeneralFunctions.waittime();
+		                 try {
+								Thread.sleep(3000);
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
 		bStatus=GeneralFunctions.clickElement(By.xpath(".//*[@id='topMenu']/div/a[3]"));
 				if(bStatus){
 		        	System.out.println("Payment Inquiry-Pass");
@@ -28,6 +117,12 @@ public class MAGIC_Q2_MA9741Test {
 		        }
 				
 				//Payment Inquiry-->Invoice Inquiry//
+				  try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 				bStatus=GeneralFunctions.clickElement(By.linkText("Invoice Inquiry"));
 				if(bStatus){
 		        	System.out.println("Invoice Inquiry-Pass");
@@ -40,7 +135,14 @@ public class MAGIC_Q2_MA9741Test {
 	@When("^Click Orange Link$")
 	public void click_Orange_Link() throws Throwable {
 		
-	bStatus=GeneralFunctions.clickElement(By.xpath(".//*[@id='invoiceABODataTable']/tbody/tr[2]/td[10]/span/b"));
+		  try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		bStatus=GeneralFunctions.clickElement(By.xpath(".//*[@id='invoiceABODataTable']/tbody/tr[2]/td[10]/span/b"));
 		if(bStatus){
         	System.out.println("Orange Link-Pass");
         }	
@@ -52,8 +154,20 @@ public class MAGIC_Q2_MA9741Test {
 	@When("^Click Amount$")
 	public void click_Amount() throws Throwable {
 		
-		 //Validating the Expected//
-		GeneralFunctions.waittime();
+		  try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		  
+		//Validating the Expected//
+		  try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		bStatus=GeneralFunctions.clickElement(By.xpath(".//*[@id='viewTableDiv']/table/tbody/tr[2]/td[4]/span/b"));
 		if(bStatus){
         	System.out.println("Amount Details-Pass");
@@ -78,7 +192,12 @@ public class MAGIC_Q2_MA9741Test {
 
 	@Then("^Click Cross$")
 	public void Click_Cross() throws Throwable {
-		GeneralFunctions.waittime();
+		 try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		bStatus=GeneralFunctions.clickElement(By.xpath(".//*[@id='div0']/table/tbody/tr[1]/td/table/tbody/tr/td[2]/img"));
 		if(bStatus){
         	System.out.println("Click on Cross-Pass");
@@ -98,4 +217,21 @@ public class MAGIC_Q2_MA9741Test {
         	System.out.println("Back-Fail");
         }
 		}
-}
+
+	@Then("^Close Screen$")
+	public void  Close_Screen() throws Throwable {
+	  
+	  	bStatus = GeneralFunctions.close();
+	      if (bStatus) {
+	           System.out.println("MAGIC Closed-Pass");
+	       } else {
+	           System.out.println("Close MAGIC-Fail");
+	       }
+	}
+	}
+
+
+
+
+
+
--- src/test/resources/cucumber/MA9741.feature
+++ src/test/resources/cucumber/MA9741.feature
@@ -1,16 +1,10 @@
 Feature: Invoice Inquiry Screen
 
- Scenario Outline:  
-         Given user navigates to GAMMA
-         And "<aff>" "<abo>" "<period>" entered and click on submit 
-         When in Invoice Inquiry Screen
-       And Click Orange Link 
+ Scenario:  
+       Given user is in Invoice Inquiry
+       When Click Orange Link 
        And Click Amount
        Then Bonus amount displays
        And Click Cross
        And Click back
-       And Close Gamma
-         
-         Examples:
-         | aff | abo | period |
-         | 010 | 9995 | 062017 |
+       And Close Screen
