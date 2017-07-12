1c59524eeac778f358444787e8f9c4a9b7e694e7
nagapg <aiudde2@amway.com>
2017-07-12 11:13:59 -0400

    Removed unwanted comments

--- src/test/java/com/alticor/magic/test/MAGIC_Q2_MA9739Test.java
+++ src/test/java/com/alticor/magic/test/MAGIC_Q2_MA9739Test.java
@@ -6,6 +6,8 @@ package com.alticor.magic.test;
 
 import org.openqa.selenium.By;
 import com.alticor.magic.GeneralFunctions;
+
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
@@ -14,11 +16,112 @@ public class MAGIC_Q2_MA9739Test {
 	/** Declare variables */
 	boolean bStatus;
 	
-	@When("^navigated to Invoice Inquiry$")
-	public void navigated_to_Invoice_Inquiry() throws Throwable {
+	@Given("^user navigates to Invoice Inquiry$")
+	public void user_navigates_to_Invoice_Inquiry() throws Throwable {
 		
+		//Navigate to Server//
+		bStatus =GeneralFunctions.openhubBrowser("http://10.72.16.9:32768/wd/hub","firefox");
+	if (bStatus) {
+				    System.out.println("Server Firefox-Pass");
+				     } else {
+				     System.out.println("Server Firefox-Fail");
+				     }
+					
+					//Navigate to MAGIC-QA//
+				 bStatus =GeneralFunctions.openapp("https://magic-gamma/EBS_UI_Web/Magic");
+				  if (bStatus) {
+				    System.out.println("MAGIC_QA-Pass");
+				    } else {
+				    System.out.println("MAGIC_QA-Fail");
+				    }
+				
+				  try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				 
+				    //Okta Login//
+				    GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='okta-signin-username']"),"cmns559");
+				    GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='okta-signin-password']"),"our49pot");
+				    GeneralFunctions.clickElement(By.xpath(".//*[@id='okta-signin-submit']"));
+				    try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+	
+//Enter Aff//
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	    bStatus =GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='selectedAffLeftContainer']"),"010");
+		  if (bStatus) {
+		System.out.println("AFF-Pass");
+		} else {
+		System.out.println("AFF-Fail");
+		}
+
+		  try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		  
+			  //Enter Abo//
+		bStatus =GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='selectedIboNumber']"),"9995");
+		  if (bStatus) {
+		System.out.println("ABO-Pass");
+		} else {
+		System.out.println("ABO-Fail");
+		}
+		  
+		  try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		  
+			   //Enter Period//
+		bStatus=GeneralFunctions.enterTextToElement(By.id("selectedPeriod"),"072017");
+		               if(bStatus){
+		System.out.println("Period-Pass");
+		}         
+		else{
+		System.out.println("Period-Fail");
+		}
+		 
+		               try {
+		   				Thread.sleep(3000);
+		   			} catch (InterruptedException e) {
+		   				// TODO Auto-generated catch block
+		   				e.printStackTrace();
+		   			}
+		   		
+		       //Click on submit//
+		                 bStatus =GeneralFunctions.clickElement(By.id("SubmitButton"));
+		                 if (bStatus) {
+		        System.out.println("Submit-Pass");
+		    } else {
+		        System.out.println("Submit-Fail");
+		    }
+		                 
+		                 try {
+		     				Thread.sleep(3000);
+		     			} catch (InterruptedException e) {
+		     				// TODO Auto-generated catch block
+		     				e.printStackTrace();
+		     			}
+		                       
 					//Click on Payment Inquiry//
-		GeneralFunctions.waittime();
 		bStatus=GeneralFunctions.clickElement(By.xpath(".//*[@id='topMenu']/div/a[3]"));
 				if(bStatus){
 		        	System.out.println("Payment Inquiry-Pass");
@@ -28,7 +131,6 @@ public class MAGIC_Q2_MA9739Test {
 		        }
 				
 				//Payment Inquiry-->Invoice Inquiry//
-				GeneralFunctions.waittime();
 				bStatus=GeneralFunctions.clickElement(By.linkText("Invoice Inquiry"));
 				if(bStatus){
 		        	System.out.println("Invoice Inquiry-Pass");
@@ -42,6 +144,7 @@ public class MAGIC_Q2_MA9739Test {
 	public void Click_orange_link() throws Throwable {
 	
 	GeneralFunctions.waittime();
+	
 	bStatus=GeneralFunctions.clickElement(By.xpath(".//*[@id='invoiceABODataTable']/tbody/tr[2]/td[10]/span/b"));
 	if(bStatus){
     	System.out.println("Orange Link-Pass");
@@ -55,7 +158,6 @@ public class MAGIC_Q2_MA9739Test {
 	public void view_bonus_amount_details() throws Throwable {
 		
 		//Validating the Expected//
-		GeneralFunctions.waittime();
 		bStatus=GeneralFunctions.clickElement(By.xpath(".//*[@id='viewTableDiv']/table/tbody/tr[2]/td[4]/span/b"));
 		if(bStatus){
         	System.out.println("Amount Details-Pass");
@@ -64,4 +166,21 @@ public class MAGIC_Q2_MA9739Test {
         	System.out.println("Amount Details-Fail");
         }
 		}
-}
+
+	@Then("^Close Invoice Inquiry$")
+	public void  Close_Invoice_Inquiry() throws Throwable {
+	  
+	  	bStatus = GeneralFunctions.close();
+	      if (bStatus) {
+	           System.out.println("MAGIC Closed-Pass");
+	       } else {
+	           System.out.println("Close MAGIC-Fail");
+	       }
+	}
+	}
+
+
+
+
+
+
--- src/test/resources/cucumber/MA9739.feature
+++ src/test/resources/cucumber/MA9739.feature
@@ -1,14 +1,7 @@
 Feature: Invoice Inquiry Screen
 
-Scenario Outline:  
-         Given user navigates to GAMMA
-         And "<aff>" "<abo>" "<period>" entered and click on submit 
-         When navigated to Invoice Inquiry
-       And Click Orange link  
+Scenario:  
+        Given user navigates to Invoice Inquiry
+        When Click Orange link  
        Then view bonus amount details
-      And Close Gamma
-         
-         Examples:
-         | aff | abo | period |
-         | 010 | 9995 | 062017 |
-    
+       And Close Invoice Inquiry
