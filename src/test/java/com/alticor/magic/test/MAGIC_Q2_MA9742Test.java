d16a0852367ea303602d4290046083140d8c95b9
nagapg <aiudde2@amway.com>
2017-07-12 12:04:31 -0400

    Code clean up

--- src/test/java/com/alticor/magic/test/MAGIC_Q2_MA9742Test.java
+++ src/test/java/com/alticor/magic/test/MAGIC_Q2_MA9742Test.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.By;
 import com.alticor.magic.GeneralFunctions;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 public class MAGIC_Q2_MA9742Test {
 
@@ -15,15 +16,119 @@ public class MAGIC_Q2_MA9742Test {
 	boolean bStatus;
 	 String plat,sponsor;
 	 
-	 @Given("^Capture Platinum value and sponsor value$")
+	 @Given("^user navigates to Dashboard$")
+		public void user_navigates_to_Dashboard() throws Throwable {
+			
+			//Navigate to Server//
+			bStatus =GeneralFunctions.openhubBrowser("http://10.72.16.9:32768/wd/hub","firefox");
+			if (bStatus) {
+					    System.out.println("Server Firefox-Pass");
+					     } else {
+					     System.out.println("Server Firefox-Fail");
+					     }
+						
+						//Navigate to MAGIC-QA//
+					 bStatus =GeneralFunctions.openapp("https://magic-gamma/EBS_UI_Web/Magic");
+					  if (bStatus) {
+					    System.out.println("MAGIC_QA-Pass");
+					    } else {
+					    System.out.println("MAGIC_QA-Fail");
+					    }
+					  
+					  try {
+							Thread.sleep(3000);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					 
+					    //Okta Login//
+					    GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='okta-signin-username']"),"cmns559");
+					    GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='okta-signin-password']"),"our49pot");
+					    GeneralFunctions.clickElement(By.xpath(".//*[@id='okta-signin-submit']"));
+					    try {
+							Thread.sleep(3000);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+		
+	//Enter Aff//
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		    bStatus =GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='selectedAffLeftContainer']"),"010");
+			  if (bStatus) {
+			System.out.println("AFF-Pass");
+			} else {
+			System.out.println("AFF-Fail");
+			}
+
+			  try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			  
+				  //Enter Abo//
+			bStatus =GeneralFunctions.enterTextToElement(By.xpath(".//*[@id='selectedIboNumber']"),"9995");
+			  if (bStatus) {
+			System.out.println("ABO-Pass");
+			} else {
+			System.out.println("ABO-Fail");
+			}
+			  
+			  try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			  
+				   //Enter Period//
+			bStatus=GeneralFunctions.enterTextToElement(By.id("selectedPeriod"),"072017");
+			               if(bStatus){
+			System.out.println("Period-Pass");
+			}         
+			else{
+			System.out.println("Period-Fail");
+			}
+			 
+			               try {
+			   				Thread.sleep(3000);
+			   			} catch (InterruptedException e) {
+			   				// TODO Auto-generated catch block
+			   				e.printStackTrace();
+			   			}
+			   		
+			       //Click on submit//
+			                 bStatus =GeneralFunctions.clickElement(By.id("SubmitButton"));
+			                 if (bStatus) {
+			        System.out.println("Submit-Pass");
+			    } else {
+			        System.out.println("Submit-Fail");
+			    }
+			                 
+			                 try {
+			     				Thread.sleep(3000);
+			     			} catch (InterruptedException e) {
+			     				// TODO Auto-generated catch block
+			     				e.printStackTrace();
+			     			}
+			               }
+	 
+			                 @Then("^Capture Platinum value and sponsor value$")
 			         		public void Capture_Platinum_value_and_sponsor_value() throws Throwable{
 		
-		 GeneralFunctions.waittime();
 			  //Capture Platinum Value from Dashboard//
 			    plat=GeneralFunctions.getText(By.xpath(".//*[@id='ABOInfo']/table/tbody/tr[3]/td[5]/a"));
 			 	System.out.println("The Platinum value is    "  +  plat);
 				
-			 	 GeneralFunctions.waittime();
 				//Capturing Sponsor value from Dashboard//
 				sponsor=GeneralFunctions.getText(By.xpath(".//*[@id='ABOInfo']/table/tbody/tr[4]/td[2]/a"));
 				System.out.println("The Sponsor value is      "  +  sponsor);
@@ -32,7 +137,6 @@ public class MAGIC_Q2_MA9742Test {
 	  @Then("^Values are same$")
 		public void Values_are_same() throws Throwable{
 	
-		  GeneralFunctions.waittime();
 				    if(plat!=null&&!plat.equals("sponsor")){
 		    	 System.out.println("###########Result#######################");
 					System.out.println("Platinum and Sponsor are equal");
@@ -45,4 +149,21 @@ public class MAGIC_Q2_MA9742Test {
 					 System.out.println("################################################");
 				}
 	  }
-}
+
+
+	  @Then("^Close Dashboard$")
+		public void  Close_Dashboard() throws Throwable {
+		  
+		  	bStatus = GeneralFunctions.close();
+		      if (bStatus) {
+		           System.out.println("MAGIC Closed-Pass");
+		       } else {
+		           System.out.println("Close MAGIC-Fail");
+		       }
+		}
+		}
+
+
+
+
+
--- src/test/resources/cucumber/MA9742.feature
+++ src/test/resources/cucumber/MA9742.feature
@@ -1,12 +1,7 @@
 Feature:  Dashboard
 
- Scenario Outline:  
-         Given user navigates to GAMMA
-         And "<aff>" "<abo>" "<period>" entered and click on submit 
-         And Capture Platinum value and sponsor value 
-      Then Values are same
-      And Close Gamma
-         
-         Examples:
-         | aff | abo | period |
-         | 010 | 9995 | 062017 |
+ Scenario:  
+        Given user navigates to Dashboard
+         Then Capture Platinum value and sponsor value 
+         And Values are same
+         And Close Dashboard
